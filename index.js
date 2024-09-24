553a7f3b91254ed11ec0e72a388bf99ba21f4bad
Mauro Ezequiel Flores <mauroezequielflores48@gmail.com>
2024-09-24 19:56:13 -0300

    uibuild

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const Note = require("./models/note");
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+let notes = [];
 
 app.use(express.static("dist"));
 
@@ -45,7 +31,9 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 const generateId = () => {
@@ -57,23 +45,32 @@ app.post("/api/notes", (request, response) => {
   const body = request.body;
 
   if (!body.content) {
+    //body.content === undefined
     return response.status(400).json({
       error: "content missing",
     });
   }
 
-  const note = {
+  const note = new Note({
+    //Note object is created by Note constructor.
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    //Respuesta dentro de la función callback para op save
+    //savedNote es la nota guardada y recien crada.
+    response.json(savedNote);
+    //Esta nota respuesta esta formateada con toJSON
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
+  //metodo findById de mongoose
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
+  /*
   const id = Number(request.params.id);
   const note = notes.find((note) => note.id === id);
   if (note) {
@@ -81,7 +78,7 @@ app.get("/api/notes/:id", (request, response) => {
   } else {
     console.log("x");
     response.status(404).end();
-  }
+  }*/
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -93,7 +90,7 @@ app.delete("/api/notes/:id", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
